ad93d52ad34a767bee9c3f8004a30ae46d723f71
asteroidkimiaa <105914389+asteroidkimiaa@users.noreply.github.com>
2022-06-02 10:12:30 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include "iostream"
+using namespace std;
+int main({}) {
+    char c;
+    bool hurufvokalK, hurufvokalB;
+
+    cout << "Masukan huruf: ";
+    cin >> c;
+
+    hurufvokalK = (c == "a")
+}
