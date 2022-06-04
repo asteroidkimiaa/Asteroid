4f7f5141cdd6f499d06eb526ca1d0edc1faea781
Rinaathiahh <106719711+Rinaathiahh@users.noreply.github.com>
2022-06-04 09:41:13 +0700

    Update molekul-ni(ii)dietilditiokarbamat.cpp
    Penambahan pada baris 12 (int counter = 0) dan perubahan pada baris 15-24 (penambahan angka pada setiap atom C)

--- molekul-ni(ii)dietilditiokarbamat.cpp
+++ molekul-ni(ii)dietilditiokarbamat.cpp
@@ -9,19 +9,19 @@ using namespace std;
 int main(){
     // deklarasi variabel
     float pi = 3.14;
+    int counter = 0;
 
     // deklarasi molekul ni(ii)dietilditiokarbamat
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
-    float Cx, Cy, Cz;
+    float C1x, C1y, C1z;
+    float C2x, C2y, C2z;
+    float C3x, C3y, C3z;
+    float C4x, C4y, C4z;
+    float C5x, C5y, C5z;
+    float C6x, C6y, C6z;
+    float C7x, C7y, C7z;
+    float C8x, C8y, C8z;
+    float C9x, C9y, C9z;
+    float C10x, C10y, C10z;
     float H1x, H1y, H1z;
     float H2x, H2y, H2z;
     float H3x, H3y, H3z;
