430214cc14ac0dc2b063d0753dac9fb6e373cef3
asteroidkimiaa <105914389+asteroidkimiaa@users.noreply.github.com>
2022-06-04 08:50:12 +0700

    Add files via upload

---
+++ molekul-ni(ii)dibutilditiokarbamat.cpp
@@ -0,0 +1,69 @@
+
+#include "iostream"
+#include "cmath"
+#include "iomanip"
+#include "fstream"
+
+using namespace std;
+
+int main(){
+    // deklarasi variabel
+    float pi = 3.14;
+
+    // deklarasi molekul ni(ii)dibutilditiokarbamat
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float Cx, Cy, Cz;
+    float H1x, H1y, H1z;
+    float H2x, H2y, H2z;
+    float H3x, H3y, H3z;
+    float H4x, H4y, H4z; 
+    float H5x, H5y, H5z;
+    float H6x, H6y, H6z;
+    float H7x, H7y, H7z;
+    float H8x, H8y, H8z;
+    float H9x, H9y, H9z;
+    float H10x, H10y, H10z;
+    float H11x, H11y, H11z;
+    float H12x, H12y, H12z;
+    float H13x, H13y, H13z;
+    float H14x, H14y, H14z;
+    float H15x, H15y, H15z;
+    float H16x, H16y, H16z;
+    float H17x, H17y, H17z;
+    float H18x, H18y, H18z;
+    float H19x, H19y, H19z;
+    float H20x, H20y, H20z;
+    float H21x, H21y, H21z;
+    float H22x, H22y, H22z;
+    float H23x, H23y, H23z;
+    float H24x, H24y, H24z;
+    float H25x, H25y, H25z;
+    float H26x, H26y, H26z;
+    float H27x, H27y, H27z;
+    float H28x, H28y, H28z;
+    float H29x, H29y, H29z;
+    float H30x, H30y, H30z;
+    float H31x, H31y, H31z;
+    float H32x, H32y, H32z;
+    float H33x, H33y, H33z;
+    float H34x, H34y, H34z;
+    float H35x, H35y, H35z;
+    float H36x, H36y, H36z;
+
+    // masukkan nilai untuk setiap variabel C18H36N2NiS4
