a7a1e87abbbbd0ce7fe5f5a29c935f6ae6e1cba5
asteroidkimiaa <105914389+asteroidkimiaa@users.noreply.github.com>
2022-06-06 21:06:17 +0700

    Add files via upload

---
+++ molekul-kloroform.cpp
@@ -0,0 +1,23 @@
+
+#include "iostream"
+#include "cmath"
+#include "iomanip"
+#include "fstream"
+
+using namespace std;
+
+int main(){
+    // deklarasi variabel
+    float pi = 3.14;
+    int counter = 0;
+
+    // deklarasi molekul kloroform
+    float CHx, CHy, CHz;
+    float Cl1x, Cl1y, CH1z;
+    float Cl2x, Cl2y, Cl2z;
+    float Cl3x, Cl3y, Cl3z;
+
+    /*           CH
+               /  | \
+              Cl Cl Cl
+            molekul kloroform itu bentuk rigid
