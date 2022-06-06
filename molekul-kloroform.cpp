73b6c832b2ca30074ef0d8275bc1638d70c21e6a
Dea Ananda Marsas Putri <105914389+asteroidkimiaa@users.noreply.github.com>
2022-06-06 21:16:49 +0700

    Update molekul-kloroform.cpp

--- molekul-kloroform.cpp
+++ molekul-kloroform.cpp
@@ -38,4 +38,32 @@ int main(){
 
     float rB_Cl3x = 0.0;
     float rB_Cl3y = cos(54.75*pi/180) * 1.090;
-    float rB_Cl3z = - sin(54.75*pi/180) * 1.090;            
+    float rB_Cl3z = - sin(54.75*pi/180) * 1.090;     
+    
+    // iterasi untuk setiap molekul kloroform
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            for(int k = 0; k < 5; k++){
+                
+                CHx[counter] = rB_CHx + (i*3.01);
+                CHy[counter] = rB_CHy + (j*3.01);
+                CHz[counter] = rB_CHz + (k*3.01);
+
+                Cl1x[counter] = CHx[counter] + rB_Cl1x;
+                Cl1y[counter] = CHy[counter] + rB_Cl1y;
+                Cl1z[counter] = CHz[counter] + rB_Cl1z;
+
+                Cl2x[counter] = CHx[counter] + rB_Cl2x;
+                Cl2y[counter] = CHy[counter] + rB_Cl2y;
+                Cl2z[counter] = CHz[counter] + rB_Cl2z;
+
+                Cl3x[counter] = CHx[counter] + rB_Cl3x;
+                Cl3y[counter] = CHy[counter] + rB_Cl3y;
+                Cl3z[counter] = CHz[counter] + rB_Cl3z;
+
+
+                 counter += 1;
+            }
+        }
+    }
+
